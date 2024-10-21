a67380bd77678de794a7961aa9ad61c0b79cfab8
mlsrinivas70 <80315375+MLSrinivas70@users.noreply.github.com>
2024-10-21 15:08:18 +0530

    Bug fixes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,12 +9,12 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDKdqldqchO7tOXMfpO6vEj15GF17dtRbs",
+  apiKey: process.env.FIREBASE_APIKEY,
   authDomain: "netflix-gpt-1a80c.firebaseapp.com",
   projectId: "netflix-gpt-1a80c",
   storageBucket: "netflix-gpt-1a80c.appspot.com",
   messagingSenderId: "933709731814",
-  appId: "1:933709731814:web:f6d20bd20f602b35ee2827",
+  appId: process.env.FIREBASE_APPID,
   measurementId: "G-X2Y4H8KQEQ"
 };
 
