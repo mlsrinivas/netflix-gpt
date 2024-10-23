6639027eb840ca570bce62a6e8105b7e7fe780cb
mlsrinivas70 <80315375+MLSrinivas70@users.noreply.github.com>
2024-10-23 16:45:26 +0530

    added logo

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookPovider = new FacebookAuthProvider();
 
 export const auth = getAuth();
--- src/views/login.jsx
+++ src/views/login.jsx
@@ -2,12 +2,22 @@ import { Button, Grid2, Input, TextField, Typography } from '@mui/material'
 import React, {useEffect, useRef, useState} from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
 import { validateInput } from '../utils/validation';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from '../utils/firebase';
+import { 
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged, 
+    signInWithEmailAndPassword, 
+    updateProfile, 
+    GoogleAuthProvider,
+    signInWithPopup,
+    FacebookAuthProvider
+} from "firebase/auth";
+import { auth, facebookPovider, googleProvider } from '../utils/firebase';
 import { useDispatch } from 'react-redux';
 import { addUser, removeUser } from '../redux/reducers/loginReducer';
 import Header from '../components/header';
 import { LOGIN_BG_IMAGE, USER_AVATAR } from '../utils/constants';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Login = () => {
 
@@ -56,6 +66,31 @@ const Login = () => {
         }        
     }
 
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, googleProvider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+        }).catch((error) => {
+            console.log(error, 'error_google_login')
+        });
+    }
+
+    const handleFacebookLogin = async() => {
+        try {
+            const result = await signInWithPopup(auth, facebookPovider);
+            const credential = FacebookAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+    
+            console.log('User Info:', user);
+            console.log('Access Token:', token);
+        } catch (error) {
+            console.error('Error during Facebook sign-in:', error);
+        }
+    }
+
     const handleFocusPassword = (event) => {
         if (event.key === "Enter") {
             password.current.focus()
@@ -78,7 +113,7 @@ const Login = () => {
             <Grid2 sx={{position: 'absolute', width: '100%'}}>
                 <Header />
             </Grid2>
-            <Grid2 sx={{position: 'absolute', top: '20%', height: isLoginView ? 350 : 420, width: 400, backgroundColor: '#000', borderRadius: 5, padding: 3}}>
+            <Grid2 sx={{position: 'absolute', top: '20%', height: isLoginView ? 390 : 420, width: 400, backgroundColor: '#000', borderRadius: 5, padding: 3}}>
                 <Typography sx={{fontSize: 30, color: '#FFF', fontWeight: 'bold'}}>{isLoginView ? 'Sign In' : 'Sign Up'}</Typography>
                 {!isLoginView &&
                     <input 
@@ -106,6 +141,19 @@ const Login = () => {
                 <Button onClick={handleSubmit} sx={{height: 40, width: '90%', borderRadius: 3, border:'1px solid red', alignSelf:'center', marginTop: 1, cursor: 'pointer'}}>
                     <Typography sx={{color:'#FFF'}}>{isLoginView ? 'Login' : 'Register'}</Typography>
                 </Button>
+
+                {isLoginView && 
+                    <Grid2 sx={{width: '90%', display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+                        <Button onClick={handleGoogleLogin} sx={{height: 40, width: '48%', borderRadius: 3, border:'1px solid red', alignSelf:'center', marginTop: 1, cursor: 'pointer'}}>
+                            <GoogleIcon sx={{color:'red', paddingRight: 1}}/>
+                            <Typography sx={{color:'#FFF'}}>{'Google'}</Typography>
+                        </Button>
+                        <Button onClick={handleFacebookLogin} sx={{height: 40, width: '48%', borderRadius: 3, border:'1px solid red', alignSelf:'center', marginTop: 1, cursor: 'pointer'}}>
+                            <FacebookIcon sx={{color:'red', paddingRight: 1}}/>
+                            <Typography sx={{color:'#FFF'}}>{'Facebook'}</Typography>
+                        </Button>
+                    </Grid2>
+                }
                 
                 <Grid2 sx={{marginTop: 2}}>
                     {!isLoginView ?
